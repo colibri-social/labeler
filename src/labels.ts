9c32bfe7cf8885450bdb67b56f97fb01de8e5298
Louis Escher <louisescher@proton.me>
2026-07-23 23:28:11 +0200

    feat: New badge types

--- src/labels.ts
+++ src/labels.ts
@@ -1,3 +1,9 @@
-export const BADGE_LABELS = ["team", "play-store-tester"] as const;
+export const BADGE_LABELS = [
+	"team",
+	"play-store-tester",
+	"backer-five",
+	"sponsor-twenty-five",
+	"donator",
+] as const;
 
 export type BadgeLabel = (typeof BADGE_LABELS)[number];
